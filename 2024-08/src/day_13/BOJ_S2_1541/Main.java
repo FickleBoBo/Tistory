e35e06b6faedae243ddf152d10fc6a3e12a1aaf8
FickleBoBo <yukmw0704@gmail.com>
2024-08-13 21:35:20 +0900

    2024 08 13 블로그 업로드

---
+++ 2024-08/src/day_13/BOJ_S2_1541/Main.java
@@ -0,0 +1,34 @@
+package day_13.BOJ_S2_1541;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
+        StringTokenizer st2 = new StringTokenizer(st.nextToken(), "+");
+
+        int sum = 0;
+
+        while(st2.hasMoreTokens()){
+            sum += Integer.parseInt(st2.nextToken());
+        }
+
+        while(st.hasMoreTokens()){
+            st2 = new StringTokenizer(st.nextToken(), "+");
+
+            int tmp = 0;
+
+            while(st2.hasMoreTokens()){
+                tmp += Integer.parseInt(st2.nextToken());
+            }
+
+            sum -= tmp;
+        }
+
+        System.out.println(sum);
+    }
+}
